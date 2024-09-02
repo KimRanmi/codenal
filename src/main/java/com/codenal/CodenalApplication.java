d7c2d353f0a82deb136025db129614f5506eaeed
SuHyeonSim <springssh24@gmail.com>
2024-09-02 14:49:38 +0900

    [심수현] 초기설정 gradle

---
+++ src/main/java/com/codenal/CodenalApplication.java
@@ -0,0 +1,13 @@
+package com.codenal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodenalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodenalApplication.class, args);
+	}
+
+}
