8dfbce919ba76d28ddfb5d0e61f1ad8db02c0d57
whddn <jongw306@gmail.com>
2024-09-03 17:25:27 +0900

    [최종우] 로그인 기능(완료)

--- src/main/java/com/codenal/CodenalApplication.java
+++ src/main/java/com/codenal/CodenalApplication.java
@@ -1,29 +1,41 @@
 package com.codenal;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.codenal.employee.domain.Employee;
+import com.codenal.employee.repository.EmployeeRepository;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class CodenalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CodenalApplication.class, args);
-		
-		// 비밀번호 인코더 생성
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		
-		// 두 개의 원시 비밀번호를 배열로 준비
-		String[] rawPasswords = {"work1234", "work1234"};
-		
-		// 각 비밀번호를 인코딩하여 출력
-		for (int i = 0; i < rawPasswords.length; i++) {
-			String encodedPassword = encoder.encode(rawPasswords[i]);
-			System.out.println("Encoded password for account " + (i+1) + ": " + encodedPassword);
-			
-			
-		}
-	}
-	
-	
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodenalApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        // 모든 직원의 비밀번호를 확인하고 암호화하여 저장
+        List<Employee> employees = employeeRepository.findAll();
+        for (Employee employee : employees) {
+            if (!employee.getEmpPw().startsWith("$2a$")) { // 비밀번호가 이미 암호화되지 않았다면
+                String rawPassword = employee.getEmpPw();
+                String encodedPassword = passwordEncoder.encode(rawPassword);
+                employee.setEmpPw(encodedPassword);
+                employeeRepository.save(employee);
+            }
+        }
+    }
 }
