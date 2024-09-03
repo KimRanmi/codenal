d03ce4cad3be65e9887196e8f9453fa969bcc04f
SuHyeonSim <springssh24@gmail.com>
2024-09-03 17:08:38 +0900

    [심수현] 게시판 목록, 게시글 상세조회 html 설

--- src/main/java/com/codenal/announce/controller/AnnounceController.java
+++ src/main/java/com/codenal/announce/controller/AnnounceController.java
@@ -12,5 +12,22 @@ public class AnnounceController {
 		return "apps/announce";
 	}
 	
+	@GetMapping("/announce_detail")
+	public String apps_projects_overview() {
+		return "apps/announce_detail";
+	}
+
+	
+//	@GetMapping("/apps-projects-list")
+//	public String apps_projects_list() {
+//		return "apps/projects-list";
+//	}
+//	
+//	
+//	@GetMapping("/apps-projects-create")
+//	public String apps_projects_create() {
+//		return "apps/projects-create";
+//	}
+	
 	
 }
