7ac9405355dde0a8ba29aa5cc31d9d7eaf048ad2
SuHyeonSim <springssh24@gmail.com>
2024-09-03 14:02:01 +0900

    [심수현]

--- src/main/java/com/codenal/announce/controller/AnnounceController.java
+++ src/main/java/com/codenal/announce/controller/AnnounceController.java
@@ -1,5 +1,16 @@
 package com.codenal.announce.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AnnounceController {
 
+	
+	@GetMapping("/announce")
+	public String apps_tasks_list_view() {
+		return "apps/announce";
+	}
+	
+	
 }
