b5510899bc3fb796d255afe5bf0876402e776ff3
KimRanmi <rmkim4@gmail.com>
2024-09-05 00:41:38 +0900

    Revert "Rmk"

--- src/main/java/com/codenal/employee/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codenal.employee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.codenal.employee.domain.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    Employee findByEmpId(int empId);
-}
