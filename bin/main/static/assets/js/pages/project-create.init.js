d8e2fcbbbaa4cebd6dc75f040882d2bc75916a03
SuHyeonSim <springssh24@gmail.com>
2024-09-10 12:31:16 +0900

    [심수현] 게시판 toastui editor 조회

--- bin/main/static/assets/js/pages/project-create.init.js
+++ bin/main/static/assets/js/pages/project-create.init.js
@@ -35,24 +35,24 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    selectRadio.addEventListener('change', () => {
-        if (selectRadio.checked) {
-            organizationChart.style.display = 'block';
-            loadOrganizationChart(); // 조직도 불러오기
-        }
-    });
-
-    // 조직도 불러오는 함수
-    function loadOrganizationChart() {
-        // 여기에 조직도를 불러오는 AJAX 요청 또는 다른 로직을 추가
-        // AJAX 요청을 통해 조직도 데이터를 가져와서 organizationChart div에 추가
-
-        // 조직도 HTML을 삽입하는 코드
-        organizationChart.innerHTML = `
-            <h3>조직도</h3>
-            <p>부서 및 직급을 선택하세요.</p>
-        `;
+selectRadio.addEventListener('change', () => {
+    if (selectRadio.checked) {
+        organizationChart.style.display = 'block';
+        loadOrganizationChart(); // 조직도 불러오기
     }
+});
+
+// 조직도 불러오는 함수
+function loadOrganizationChart() {
+	// 여기에 조직도를 불러오는 AJAX 요청 또는 다른 로직을 추가
+	// AJAX 요청을 통해 조직도 데이터를 가져와서 organizationChart div에 추가
+
+	// 조직도 HTML을 삽입하는 코드
+	organizationChart.innerHTML = `
+	<h3>조직도</h3>
+	<p>부서 및 직급을 선택하세요.</p>
+	`;
+}
 });
 
 
@@ -64,7 +64,12 @@ const fileInput = document.getElementById("file");
 
 form.addEventListener('submit', (e) => {
     e.preventDefault(); // 기본 폼 제출 동작 방지
-
+    // 유효성 검사
+	if (editor.getMarkdown().length < 1) {
+	    alert('에디터 내용을 입력해 주세요.');
+	    throw new Error('editor content is required!');
+	}
+	
     // FormData 객체를 생성하여 폼 데이터를 가져옴
     const formData = new FormData(form);
 
