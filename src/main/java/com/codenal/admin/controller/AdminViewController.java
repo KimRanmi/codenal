d31b461ccc15090c9716bbd4557470a894a2c261
A <A@192.168.219.197>
2024-09-22 01:01:48 +0900

    퇴사 완료, 퇴사자 구분 완료

--- src/main/java/com/codenal/admin/controller/AdminViewController.java
+++ src/main/java/com/codenal/admin/controller/AdminViewController.java
@@ -66,140 +66,146 @@ public class AdminViewController {
 
 	// 직원 정보 상세 조회
 	@GetMapping("/detail/{empId}")
-	
+
 	public String employeeListDetail(@PathVariable("empId") Long empId, 
 			Model model) {
+		
 		EmployeeDto employeeDetail = adminService.employeeDetail(empId);
 		model.addAttribute("employeeDetail", employeeDetail);
 
+		// 부서 셀렉트 박스
 		List<DepartmentsDto> departments = adminService.getAllDepartments();
 		model.addAttribute("departments", departments);
 
+		// 퇴사자는 임시 비밀번호 발급, 수정, 퇴사 설정 불가
+		String empStatus = employeeDetail.getEmpStatus();
+		model.addAttribute("empStatus", empStatus);
+
 		return "admin/detail";
 	}
-	
+
 	// 직원 정보 상세 조회 (퇴사하고 JSON 반환)
 	@GetMapping("/detail/{empId}/json")
 	@ResponseBody
 	public ResponseEntity<?> employeeListDetailJson(@PathVariable("empId") Long empId) {
-	    EmployeeDto employeeDetail = adminService.employeeDetail(empId);
+		EmployeeDto employeeDetail = adminService.employeeDetail(empId);
 
-	    Map<String, Object> response = new HashMap<>();
-	    response.put("success", true);
-	    response.put("employeeDetail", employeeDetail);
+		Map<String, Object> response = new HashMap<>();
+		response.put("success", true);
+		response.put("employeeDetail", employeeDetail);
 
-	    return ResponseEntity.ok(response); // JSON 응답 반환
-	    }
-	
-	
+		return ResponseEntity.ok(response); // JSON 응답 반환
+	}
+
+
 	// 직원 비밀번호 변경 (work1234)
 	@PostMapping("/reset-password")
 	@ResponseBody
 	public Map<String, Object> resetPassword(@RequestBody Map<String, String> requestData) {
-	    Map<String, Object> response = new HashMap<>();
-	    
-	    String adminPw = requestData.get("adminPw");
-	    Long empId = Long.parseLong(requestData.get("empId"));
-	    
-	    // 관리자 비밀번호 확인 
-	    if (!adminPw.equals("work1234")) {
-	        response.put("success", false);
-	        response.put("message", "관리자 비밀번호가 일치하지 않습니다.");
-	        return response;
-	    }
-	    
-	    // 직원 비밀번호 변경
-	    try {
-	        adminService.resetEmployeePassword(empId, "work1234");
-	        response.put("success", true);
-	    } catch (Exception e) {
-	        response.put("success", false);
-	        response.put("message", "비밀번호 변경 중 오류가 발생했습니다.");
-	    }
-	    
-	    return response;
-	}
-	
-	
-	// 직원 퇴사 
-		@PostMapping("/emp-end")
-		@ResponseBody
-		public Map<String, Object> emdEndDatePwa(@RequestBody Map<String, String> requestData) {
-		    Map<String, Object> response = new HashMap<>();
-		    
-		    String adminPw= requestData.get("adminPw");
-		    Long empId = Long.parseLong(requestData.get("empId"));
-		    String empEndDateStr = requestData.get("empEnd");
-		
-		    // 관리자 비밀번호 확인 
-		    if (!adminPw.equals("work1234")) {
-		        response.put("success", false);
-		        response.put("message", "관리자 비밀번호가 일치하지 않습니다.");
-		        return response;
-		    }
-		    
-		    if (empEndDateStr == null || empEndDateStr.isEmpty()) {
-		        response.put("success", false);
-		        response.put("message", "퇴사일이 입력되지 않았습니다.");
-		        return response;
-		    }
-	
-		    try {
-		    	// 퇴사일 String -> Date
-		        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		        LocalDate empEndDate = LocalDate.parse(empEndDateStr, formatter);
+		Map<String, Object> response = new HashMap<>();
 
-		        // empStatus 'N' 변경
-		        boolean success = adminService.emdEndDate(empId, empEndDate);
+		String adminPw = requestData.get("adminPw");
+		Long empId = Long.parseLong(requestData.get("empId"));
 
-		        if (success) {
-		            response.put("success", true);
-		            response.put("message", "직원이 성공적으로 퇴사 처리되었습니다.");
-		        } else {
-		            response.put("success", false);
-		            response.put("message", "퇴사 처리 중 오류가 발생했습니다.");
-		        }
-		    } catch (DateTimeParseException e) {
-		        response.put("success", false);
-		        response.put("message", "퇴사일 형식이 올바르지 않습니다.");
-		    }
-		    
-		    return response;
+		// 관리자 비밀번호 확인 
+		if (!adminPw.equals("work1234")) {
+			response.put("success", false);
+			response.put("message", "관리자 비밀번호가 일치하지 않습니다.");
+			return response;
 		}
+
+		// 직원 비밀번호 변경
+		try {
+			adminService.resetEmployeePassword(empId, "work1234");
+			response.put("success", true);
+		} catch (Exception e) {
+			response.put("success", false);
+			response.put("message", "비밀번호 변경 중 오류가 발생했습니다.");
+		}
+
+		return response;
+	}
+
+
+	// 직원 퇴사 
+	@PostMapping("/emp-end")
+	@ResponseBody
+	public Map<String, Object> emdEndDatePwa(@RequestBody Map<String, String> requestData) {
+		Map<String, Object> response = new HashMap<>();
+
+		String adminPw= requestData.get("adminPw");
+		Long empId = Long.parseLong(requestData.get("empId"));
+		String empEndDateStr = requestData.get("empEnd");
+
+		// 관리자 비밀번호 확인 
+		if (!adminPw.equals("work1234")) {
+			response.put("success", false);
+			response.put("message", "관리자 비밀번호가 일치하지 않습니다.");
+			return response;
+		}
+
+		if (empEndDateStr == null || empEndDateStr.isEmpty()) {
+			response.put("success", false);
+			response.put("message", "퇴사일이 입력되지 않았습니다.");
+			return response;
+		}
+
+		try {
+			// 퇴사일 String -> Date
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			LocalDate empEndDate = LocalDate.parse(empEndDateStr, formatter);
+
+			// empStatus 'N' 변경
+			boolean success = adminService.emdEndDate(empId, empEndDate);
+
+			if (success) {
+				response.put("success", true);
+				response.put("message", "직원이 성공적으로 퇴사 처리되었습니다.");
+			} else {
+				response.put("success", false);
+				response.put("message", "퇴사 처리 중 오류가 발생했습니다.");
+			}
+		} catch (DateTimeParseException e) {
+			response.put("success", false);
+			response.put("message", "퇴사일 형식이 올바르지 않습니다.");
+		}
+
+		return response;
+	}
 
 
 	// 직원 정보 수정
 	@GetMapping("/update/{empId}")
 	public String employeeUpdate(@PathVariable("empId") Long empId, Model model) {
-		
-	    EmployeeDto employeeUpdate = adminService.employeeDetail(empId);
-	    
-	    System.out.println("employeeUpdate deptNo: " + employeeUpdate.getDeptNo());
-	    model.addAttribute("employeeUpdate", employeeUpdate);
-	    
-	    // 부서 셀렉트
-	    List<DepartmentsDto> departments = adminService.getAllDepartments();
-	    model.addAttribute("departments", departments);
 
-	    // 직급 목록 추가
-	    List<JobsDto> jobs = adminService.getAllJobs();
-	    model.addAttribute("jobs", jobs);
+		EmployeeDto employeeUpdate = adminService.employeeDetail(empId);
 
-	    return "admin/update";
+		System.out.println("employeeUpdate deptNo: " + employeeUpdate.getDeptNo());
+		model.addAttribute("employeeUpdate", employeeUpdate);
+
+		// 부서 셀렉트
+		List<DepartmentsDto> departments = adminService.getAllDepartments();
+		model.addAttribute("departments", departments);
+
+		// 직급 목록 추가
+		List<JobsDto> jobs = adminService.getAllJobs();
+		model.addAttribute("jobs", jobs);
+
+		return "admin/update";
 	}
 
 	// 직원 정보 수정 (POST 요청)
 	@PostMapping("/update/{empId}")
 	public String saveEmployeeUpdate(@PathVariable("empId") Long empId, EmployeeDto employeeDto) {
-	    System.out.println("Received empId: " + empId);
-	    System.out.println("Received deptNo: " + employeeDto.getDeptNo());
+		System.out.println("Received empId: " + empId);
+		System.out.println("Received deptNo: " + employeeDto.getDeptNo());
 
-	    try {
-	        adminService.employeeUpdate(empId, employeeDto);
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	    return "redirect:/admin/detail/" + empId;
+		try {
+			adminService.employeeUpdate(empId, employeeDto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "redirect:/admin/detail/" + empId;
 	}
 
 
